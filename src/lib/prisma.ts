3118648f9f906b7d229d07704e1fa0b71a82b3cf
titusfleming <titoflemingii@gmail.com>
2025-01-01 22:05:33 -0600

    please work

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  })
-}
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+const prisma = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+  global.prisma = prisma
 }
+
+export { prisma }
